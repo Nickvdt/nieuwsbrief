5bcff10a86e788d019ca200ad53450257efb472f
Nick <32934@ma-web.nl>
2023-10-12 10:53:11 +0200

    Mail wordt nu verzonden naar de database

--- public/api.php
+++ public/api.php
@@ -1,1 +1,37 @@
-// hier komt de api
+<?php
+
+include_once("./connection.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+    $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $nieuwsbrief = mysqli_real_escape_string($conn, $_POST['nieuwsbrief']);
+
+    if (empty($fname) || empty($lastname) || empty($email) || empty($nieuwsbrief)) {
+        echo "Please fill in all fields.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format.";
+    } else {
+
+        $query = "SELECT * FROM email WHERE email='$email'";
+        $result = mysqli_query($conn, $query);
+        
+        if (mysqli_num_rows($result) > 0) {
+            echo "Email already subscribed.";
+        } else {
+          
+            $sql = "INSERT INTO email (fname, lastname, email, nieuwsbrief) 
+                    VALUES ('$fname', '$lastname', '$email', '$nieuwsbrief')";
+            
+            if ($conn->query($sql) === TRUE) {
+                echo "Thank you for subscribing!";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    }
+} else {
+    echo "Invalid request method.";
+}
--- public/source/forms.php
+++ public/source/forms.php
@@ -1,13 +1,18 @@
-<form id="optInForm" method="post" action="./api.php">
+<form id="optInForm" method="POST" action="./api.php">
     <div>
         <label for="email">Email</label>
-        <input id="email" type="email" required>
+        <input class="input" type="email" name="email" placeholder="Email">
     </div>
     <div>
-        <label for="name">Name</label>
-        <input id="name" type="name" required>
+        <label for="fname">Name</label>
+        <input class="input" type="text" name="fname" placeholder="Naam" required>
     </div>
-    <select id="nieuwsbrief">
+    <div>
+        <label for="lastname"></label>
+        <input class="input" type="text" name="lastname" placeholder="Achternaam" required>
+    </div>
+    
+    <select name="nieuwsbrief" id="nieuwsbrief">
         <option value="Telegraaf">Telegraaf</option>
         <option value="ad">AD.nl</option>
     </select>
